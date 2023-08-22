ee188d9a4d4439586d1f8200613a1378a083e684
RiOuO <128416852+RiOuO@users.noreply.github.com>
2023-08-22 11:08:24 +0800

    update

---
+++ NSL_MvcCore_Draft/Nsl_Core/Nsl_Core/Controllers/Back/BackHomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nsl_Core.Models.Infra;
+
+namespace NSL_html.Controllers.Back
+{
+    [TypeFilter(typeof(AdminVerify))]
+    public class BackHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView();//不顯示Layout
+        }
+
+        public IActionResult Login()
+        {
+            return PartialView();
+        }
+		public IActionResult Signout()
+		{
+			HttpContext.Response.Cookies.Delete("Login");
+			TempData.Remove("Login");
+			return RedirectToAction("Index","NSL");
+		}
+	}
+}
---
+++ NSL_MvcCore_Draft/Nsl_Core/Nsl_Core/Controllers/Back/BackReportController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nsl_Core.Models.Infra;
+
+namespace NSL_html.Controllers.Back
+{
+    [TypeFilter(typeof(AdminVerify))]
+    public class BackReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
